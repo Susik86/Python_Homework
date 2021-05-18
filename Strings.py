2872367b403a052a81d6d8a3056b588e80dfe008
susanna.karapetyan <susanna.karapetyan@volo.global>
2021-05-18 16:22:19 +0400

    Initial commit

---
+++ Strings.py
@@ -0,0 +1,82 @@
+# Task 1
+
+text = "why is this language called python? it gets its name from the british comedy crew," \
+             " called Monty Python."
+
+
+capitalized_text = text.swapcase()
+print(capitalized_text)
+
+
+
+
+# # Task 2
+#
+#
+# text = """
+# ​
+# "THIS PART SHOULD BE IN LOWER CASE. this part should be in upper case."
+# ​
+# """
+#
+#
+# new_text = str[0:32].lower(text)
+# print(new_text)
+
+
+
+
+
+
+# Task 3
+
+
+
+#
+#
+# ni_string = """Knights of Ni: Ni!  Ni!  Ni!  Ni!  Ni!  Ni!
+#                 Arthur:        Who are you?
+#                 Knight of Ni:  We are the Knights who say.....   "Ni"!
+#                 Arthur:        (horrified) No!  Not the Knights who say "Ni"!
+#                 Knight of Ni:  The same.
+#                 Other Knight of Ni: Who are we?
+#                 Knight of Ni:  We are the keepers of the sacred words: Ni, Ping, and Nee-womm!
+#                 Other Knight of Ni: Nee-womm!
+#                 Arthur:        (to Bedevere) Those who hear them seldom live to tell the tale!
+#                 Knight of Ni: The knights who say "Ni" demand..... a sacrifice!
+#                 Arthur:       Knights of Ni, we are but simple travelers who seek the enchanter
+#                               who lives beyond these woods.
+#                 Knights of Ni: Ni!  Ni!  Ni!  Ni!  Ni!  Ni!  Ni!  Ni!  Ni!
+#                 Bedevere:      No! Noooo!  Aaaugh!  No!
+#                 Knight of Ni:  We shall say "Ni" again to you... if you do not appease us.
+#                 Arthur:        Well what is it you want?
+#                 Knight of Ni:  We want a Shrubbery!"""
+#
+# count_of_ni = ni_string.count("Ni")
+# print(ni_string)
+#
+#
+#
+# Task 5
+
+lower_string = "mony python's the meaning of life"
+new_text = lower_string.title()
+print(new_text)
+
+
+
+#
+#
+# # Task 7
+#
+# txt = "Hi May!"
+# new_text = txt.maketrans("May", "Joy")
+# print(txt.translate(new_text))
+#
+#
+#
+# # Task 8
+#
+# lst = ['I', 'am', 'King', 'Arthur', 'of', 'Camelot']
+# new_list = lst.join
+# print(new_list)
---
+++ Task1.py
@@ -0,0 +1,77 @@
+# Task 1
+
+
+fahrenheit = "F"
+celsius = "C"
+t = '96.8F'
+c = '36C'
+
+
+if fahrenheit in t:
+    value_stripped = t.strip("F")
+    t_value = float(value_stripped)
+    converted_celsius = (5 / 9) * (t_value - 32)
+    print(f"{t} in Celsius units is {converted_celsius}")
+
+
+elif celsius in c:
+    value_stripped_c = c.strip("C")
+    c_value = int(value_stripped_c)
+    converted_celsius_c = (c_value - 32) / 1.8
+    print(f"{c} in Fahrenheit units is {converted_celsius_c }")
+
+
+
+
+# Task 2
+
+
+number = 15
+
+if number/5 and number/3:
+    print("FizzBuzz")
+
+elif number/3:
+    print("Buzz")
+
+elif number/5:
+    print("Fizz")
+
+else:
+    print(number)
+
+
+
+
+# Task 3
+
+grades = {'Math': 15,
+          'Physics': 11,
+          'Geography': 18,
+          'History': 20,
+          'Geometry': 19}
+
+
+values = grades.values()
+total_sum = sum(values)
+print(total_sum)
+
+
+if total_sum <= 40:
+    print("Fail")
+
+elif 41 <= total_sum <= 60:
+    print("Satisfactory")
+
+elif 61 <= total_sum <= 80:
+    print("Good")
+
+elif 81 <= total_sum <= 100:
+    print("Outstanding")
+
+elif total_sum > 100 or total_sum < 0:
+    print("Something's wrong with the input")
+
+else:
+    print(total_sum)
+
